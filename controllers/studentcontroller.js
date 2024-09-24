025aa8d6d26506759088c31a536c9e37da804918
tarunkumar9923 <saintarun723@gmail.com>
2024-09-24 15:04:28 +0530

    update

---
+++ controllers/studentcontroller.js
@@ -0,0 +1,69 @@
+const Student = require('../models/Students');
+async function addStudent(req,res){
+    try{
+        console.log(req.body);
+       let student = new Student(req.body);
+       await student.save();
+    //    res.render('dataaddedresponse');
+    res.render('studentinsert');
+
+    }catch(err){
+        console.error(err);
+    }
+}
+async function getStudents(req,res){
+    try{
+       
+       let students = await Student.find({});
+    //    res.send(students)
+    res.render('studentlist',{
+        students:students
+
+    })
+ }catch(err){
+        console.error(err);
+    }
+}
+async function getStudentForEdit(req,res){
+    try{
+       
+    //    let id = await Student.findOne({});
+       let id = req.params.id;
+        console.log(id);
+        let student = await Student.findOne({_id:id});
+        console.log(student)
+        // res.send(student)
+        res.render('studentforupdate',{
+            student:student
+        })
+    }catch(err){
+        console.error(err);
+    }
+}
+
+async function updateStudent(req,res){
+    try{
+       let id = req.params.id;
+        // console.log(id);
+        let student = await Student.findOne({_id: id });
+        console.log(student,'student')
+        // res.send(student)
+        // res.render('studentforupdate',{
+        //     student:student
+        // })
+        student.rollNo=req.body.rollNo;
+        student.firstName=req.body.firstName;
+        student.lastName=req.body.lastName;
+        student.fatherName=req.body.fatherName;
+        student.adharCardNo=req.body.adharCardNo;
+        student.mobileNo=req.body.mobileNo;
+        await student.save();
+        res.end("<h1>Student has been update sucessfully</h1>")
+    }catch(err){
+        console.error(err);
+    }
+}
+
+module.exports={
+    addStudent ,getStudents,getStudentForEdit,updateStudent
+}
---
+++ routes/student.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router =  express.Router();
+router.use(express.urlencoded({ extended: false}));
+const studentcontroller = require('../controllers/studentcontroller');
+router.get('/',(req,res)=>{
+    res.render('home');
+})
+router.post('/add/student',(req,res)=>{
+    studentcontroller.addStudent(req,res);
+})
+router.get('/students',(req,res)=>{
+    studentcontroller.getStudents(req,res);
+})
+router.get('/edit/student/page/:id',(req,res)=>{
+    studentcontroller.getStudentForEdit(req,res)
+})
+router.post('/update/student/:id',(req,res)=>{
+    studentcontroller.updateStudent(req,res)
+
+});
+module.exports = router;
